77f4330119413faaa4084f13de2e9a3d058d8670
rodlin <im.rod@outlook.com>
2017-08-29 23:23:35 +0800

    add captcha

--- meowurl/captcha.py
+++ meowurl/captcha.py
@@ -5,10 +5,10 @@ import json
 from meowurl import app
 from geetest import GeetestLib as Geetest
 
-if not app.config["CAPTCHA_GEETEST"]:
-    raise Exception("no configuration for geetest captcha")
-
 def reg():
+    if not "CAPTCHA_GEETEST" in app.config:
+        raise Exception("no configuration for geetest captcha")
+    
     gt = Geetest(*app.config["CAPTCHA_GEETEST"])
     not_offline = gt.pre_process()
     
@@ -23,6 +23,9 @@ def reg():
     return obj
     
 def verify(challenge, validate, seccode, offline=0):
+    if not "CAPTCHA_GEETEST" in app.config:
+        return True
+    
     gt = Geetest(*app.config["CAPTCHA_GEETEST"])
     
     result = (gt.success_validate if not offline else gt.failback_validate) \
